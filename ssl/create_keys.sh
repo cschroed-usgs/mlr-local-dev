f31a1a3e738f88f4bb3973b1be957edc3bad7aa2
Zack Moore <zmoore@usgs.gov>
2020-03-10 11:45:31 -0500

    Removed uneeded command.

--- ssl/create_keys.sh
+++ ssl/create_keys.sh
@@ -22,4 +22,3 @@ openssl x509 -req \
   -days 9999 -in $DIR/wildcard.csr -signkey $DIR/wildcard.key  -out $DIR/wildcard.crt
 openssl pkcs12 -export -in $DIR/wildcard.crt -inkey $DIR/wildcard.key -name tomcat -out $DIR/tomcat.pkcs12 -password pass:changeit
 keytool -v -importkeystore -deststorepass changeit -destkeystore $DIR/keystore -deststoretype JKS -srckeystore $DIR/tomcat.pkcs12 -srcstorepass changeit -srcstoretype PKCS12 -noprompt
-chmod +r $DIR/wildcard.key
