f3196a03ff2d86aa581c5b74462c99e8a342362c
zmoore-usgs <zmoore@usgs.gov>
2020-08-21 09:38:41 -0500

    Moved Mock S3 from port 80 to port 8080 to reduce potential conflicts with system things. Also added a curl version of the script to pull remote reference lists from S3.

--- launch_backing_services.sh
+++ launch_backing_services.sh
@@ -9,7 +9,7 @@ launch_services () {
 }
 
 create_s3_bucket () {
-  curl --silent --request PUT "http://${DOCKER_ENGINE_IP}:80/mock-bucket-test"
+  curl --silent --request PUT "http://${DOCKER_ENGINE_IP}:8080/mock-bucket-test"
 }
 
 echo "Launching MLR Backing Services..."
@@ -21,7 +21,7 @@ if [[ $EXIT_CODE -ne 0 ]]; then
 fi
 
 echo "Waiting for S3 Mock to come up..."
-until nc -vzw 2 $DOCKER_ENGINE_IP 80 &>/dev/null ; do echo -ne . && sleep 2; done
+until nc -vzw 2 $DOCKER_ENGINE_IP 8080 &>/dev/null ; do echo -ne . && sleep 2; done
 echo
 echo "Creating test S3 bucket..."
 EXIT_CODE=$(create_s3_bucket)
